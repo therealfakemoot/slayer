9b89cb1b1cb9296c8c91d49b8f4186038ca07d18
Nick Dumas <nicholas.dumas@homes.com>
2018-11-21 14:31:52 +0000

    Remove main, transition to traditional go cmd/ dirs.

--- cmd/enforce/main.go
+++ cmd/enforce/main.go
@@ -3,23 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	jira "github.com/andygrunwald/go-jira"
 	log "github.com/sirupsen/logrus"
+
+	conf "git.ndumas.com/ndumas/slayer/conf"
+	sla "git.ndumas.com/ndumas/slayer/sla"
 )
 
 // BASE is the URL for the homes.com Jira instance.
 const BASE = "https://homesmediasolutions.atlassian.net"
 
 func main() {
-	// conf := flag.String("config", ".slayer", "config file path")
+	conf := flag.String("config", ".slayer", "config file path")
 	user := flag.String("user", "", "username")
 	token := flag.String("token", "", "auth token")
 	debug := flag.Bool("debug", false, "debug mode")
 	flag.Parse()
 
+	if *conf == "" {
+		log.Error("no config provided")
+	}
+
+	f, err := os.Open(conf)
+
 	if *debug {
 		log.SetLevel(log.DebugLevel)
+	}
+
+	rules, err := LoadRules(f)
+	if err != nil {
+		log.WithError(err).Error("unable to load rules")
 	}
 
 	tp := jira.BasicAuthTransport{
@@ -32,7 +47,9 @@ func main() {
 		"base":  BASE,
 		"token": token,
 	})
+}
 
+func xxxxxx() {
 	jc, err := jira.NewClient(tp.Client(), BASE)
 	if err != nil {
 		authCtx.WithError(err).Error("unable to create client")
