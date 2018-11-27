bb9a85afcb8b8ddec5087f898353a1989442b967
Nick Dumas <nicholas.dumas@homes.com>
2018-11-27 17:37:16 +0000

    First pass at the vaguely final shape the code's gonna take
    Here in the actual cmd, it all comes together. All the top level pieces
    are together. I'm printing out a ComplianceReport at the end, it's just
    empty.
    
    That requires issues.Get to return a non-empty array of Issues. Should
    be relatively straightforward.

--- cmd/enforce/main.go
+++ cmd/enforce/main.go
@@ -5,65 +5,56 @@ import (
 	"fmt"
 	"os"
 
-	jira "github.com/andygrunwald/go-jira"
 	log "github.com/sirupsen/logrus"
 
-	conf "git.ndumas.com/ndumas/slayer/conf"
+	// conf "git.ndumas.com/ndumas/slayer/conf"
+	issues "git.ndumas.com/ndumas/slayer/issues"
 	sla "git.ndumas.com/ndumas/slayer/sla"
 )
 
-// BASE is the URL for the homes.com Jira instance.
-const BASE = "https://homesmediasolutions.atlassian.net"
-
 func main() {
-	conf := flag.String("config", ".slayer", "config file path")
-	user := flag.String("user", "", "username")
-	token := flag.String("token", "", "auth token")
-	debug := flag.Bool("debug", false, "debug mode")
+	config := *flag.String("config", "conf.toml", "config file path")
+	debug := *flag.Bool("debug", false, "debug mode")
 	flag.Parse()
 
-	if *conf == "" {
-		log.Error("no config provided")
-	}
-
-	f, err := os.Open(conf)
-
-	if *debug {
+	if debug {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	rules, err := LoadRules(f)
+	if config == "" {
+		log.Error("no config provided")
+	}
+
+	f, err := os.Open(config)
+
+	if err != nil {
+		log.WithError(err).Error("unable to open rule file")
+	}
+
+	conf, err := sla.Load(f)
 	if err != nil {
 		log.WithError(err).Error("unable to load rules")
 	}
 
-	tp := jira.BasicAuthTransport{
-		Username: *user,
-		Password: *token,
+	var cr sla.ComplianceReport
+	for name, target := range conf.Targets {
+		targetCtx := log.WithFields(log.Fields{
+			"name":   name,
+			"target": target,
+		})
+		ic := make(sla.IssueCompliance)
+		// this may be the time to start using errgroup and goroutines
+		allIssues, err := issues.Get(target, conf.Auth)
+		if err != nil {
+			targetCtx.WithError(err).Error("unable to fetch issues")
+		}
+		for _, rule := range target.Rules {
+			for _, i := range allIssues {
+				ic[rule.Key()] = rule.Check(i)
+			}
+		}
+		fmt.Printf("%s: %+v\n", name, target)
 	}
 
-	authCtx := log.WithFields(log.Fields{
-		"user":  user,
-		"base":  BASE,
-		"token": token,
-	})
-}
-
-func xxxxxx() {
-	jc, err := jira.NewClient(tp.Client(), BASE)
-	if err != nil {
-		authCtx.WithError(err).Error("unable to create client")
-		return
-	}
-
-	issues, r, err := jc.Issue.Search("project = RMXC", nil)
-	if err != nil {
-		authCtx.WithFields(log.Fields{
-			"request":  fmt.Sprintf("%+v", r.Request),
-			"response": fmt.Sprintf("%+v", r),
-		}).WithError(err).Error("unable to fetch issues")
-		return
-	}
-
-	fmt.Println(issues[0])
+	fmt.Printf("%+v", cr)
 }
