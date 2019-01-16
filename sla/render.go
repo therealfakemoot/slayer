a1e68128861afa006b8e4a7b9bfaea15a536c0b8
Nick Dumas <nicholas.dumas@homes.com>
2019-01-16 19:45:00 +0000

    Reorganizing structures.
    If I can get these interfaces right, I can make the core business logic
    loop completely agnostic to the actual issue types involved.

---
+++ sla/render.go
@@ -0,0 +1,72 @@
+package sla
+
+import (
+	"io"
+	"strings"
+	"text/tabwriter"
+	"text/template"
+
+	// jira "github.com/andygrunwald/go-jira"
+	log "github.com/sirupsen/logrus"
+)
+
+/*
+var HeaderTemplate = template.Must(template.New("header").Parse("{{$sep := .Separator}}{{ range .Columns }}{{$sep}}{{.}}{{ end }}{{$sep}}\n"))
+var RowTemplate = template.Must(template.New("row").Parse("{{$sep := .Separator}}{{$sep}}{{.Issue.Key}}{{$sep}}{{.Issue.Fields.Summary}}{{$sep}}\n"))
+*/
+
+type ReportRenderer interface {
+	Render(ComplianceReport) string
+}
+
+type TermRenderer struct {
+	Out io.Writer
+}
+
+var funcMap = template.FuncMap{
+	"strip": func(s string) string {
+		return strings.TrimSpace(s)
+	},
+	"trunc": func(s string, n int) string {
+		return s[:n]
+	},
+}
+
+var TableTemplate = template.Must(template.New("root").Funcs(funcMap).Parse(`{{ $.Separator }}{{ range .Columns }}{{.}}{{ $.Separator }}{{ end }}
+{{ range $key, $ic := .Issues }}{{ $.Separator }}{{$key}}{{ $.Separator }}{{ range $rule, $pass := $ic.Rules }}{{$pass}}{{  $.Separator }}{{ end }}
+{{end}}
+`))
+
+func (tr *TermRenderer) Render(cr ComplianceReport) (s string) {
+	var (
+		tw tabwriter.Writer
+	)
+
+	tw.Init(tr.Out, 15, 4, 0, ' ', 0)
+	defer tw.Flush()
+
+	/*
+		var issues []jira.Issue
+		for _, v := range cr {
+			issues = append(issues, v.Issue)
+		}
+	*/
+
+	templateData := struct {
+		Columns   []string
+		Issues    ComplianceReport
+		Separator string
+	}{
+		Columns:   []string{"Key", "Updated", "Group"},
+		Separator: "\t",
+		Issues:    cr,
+	}
+
+	err := TableTemplate.Execute(&tw, templateData)
+
+	if err != nil {
+		log.WithError(err).Error("unable to render table")
+	}
+
+	return s
+}
