5ed6f49bccfac44dd4d783586e08e74cf5ca3955
Nick Dumas <nicholas.dumas@homes.com>
2019-01-17 15:32:24 +0000

    mime command now loads and displays config info.
    First phase. Next phase is to load issues, and then process rules on
    them.

--- cmd/mime/main.go
+++ cmd/mime/main.go
@@ -4,27 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
-	client "github.com/therealfakemoot/slayer/client"
+	conf "github.com/therealfakemoot/slayer/conf"
 )
 
 func main() {
-	var username = flag.String("user", "", "jira username")
-	var token = flag.String("token", "", "jira token")
-
-	var board = flag.Int("board", 0, "filter id")
-	var filter = flag.Int("filter", 0, "board id")
+	var config = flag.String("config", "conf.toml", "config file")
 
 	flag.Parse()
 
-	log.Printf("username: %s", username)
-	log.Printf("token: %s", token)
-
-	if *board != 0 && *filter != 0 {
-		log.Fatal("please select ONLY board or filter, not both")
+	f, err := os.Open(*config)
+	if err != nil {
+		log.Fatal("could not open config file")
 	}
+	auth := conf.LoadAuth(f)
 
-	auth := client.AuthOptions{User: *username, Token: *token}
-
-	fmt.Println(auth)
+	fmt.Printf("%#v\n", auth)
 }
